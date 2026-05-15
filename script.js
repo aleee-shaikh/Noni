eeaba87cd0e9b3e54e73a0ade39476beb95894ad
Muhammad Ali <52410543+aleee-shaikh@users.noreply.github.com>
2026-05-15 15:34:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-$(document).ready(function() {
-  $('#contactForm').on('submit', function(e) {
-    e.preventDefault();
-    $('.form-status').text('Thank you for your message!').css('color', '#00ffff');
-    this.reset();
-  });
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+    a.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(a.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
 });
